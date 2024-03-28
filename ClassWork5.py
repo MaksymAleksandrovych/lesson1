200ec7607a9a8d3c9eb63bee6dbfbfd96dc7d941
Maksym <maksymaleksandrovych11@gmail.com>
2024-03-28 16:18:37 +0200

    Homework5

---
+++ ClassWork5.py
@@ -0,0 +1,29 @@
+# import inspect
+# import requests
+# help(requests)
+#
+# def first_function():
+#     pass
+#
+# class Human:
+#     pass
+# rq = random
+# first_f = first_function
+# nick = Human
+# print(random.__name__)
+# print(rq.__name__)
+# print(first_function.__name__)
+# print(first_f.__name__)
+# print(Human.__name__)
+# print(nick.__name__)
+# list=[]
+# print=[type()]
+# \
+
+# def checker(var_1)
+#     if type(var_1)!= str:
+#         raise TypeError(f"Sorry,we can't workwith {type(var_1)}, "f"we need class str")
+#     else:
+#     return var_1
+# first_var = "10"
+# checker(first_var)
---
+++ Homework5.py
@@ -0,0 +1,7 @@
+pip install colorama
+    import colorama
+dir(colorama)
+init(autoreset=False, strip=None, wrap=True, convert=None, **kwargs):
+colorama.Fore.BLUE
+colorama.Style.DIM
+colorama.Bacck.GRAY
